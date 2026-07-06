eb40e0d17777fea43ee9e28d7024166d056d881d
DRNKN Master <master@drnkn.dev>
2026-07-06 16:43:41 +0700

    refactor(api): dedupe withinGracePeriod, delete dead getQuotaForDevice

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,7 +1,7 @@
 export { createAuth, authenticateRequest, createAuthMiddleware, generateDeviceToken, sha256Hex } from "./auth/index.js";
 export type { AuthResult, CoreAuth, CoreDb, CoreSchema, Principal } from "./auth/index.js";
 export { createDeviceAuthRouter } from "./auth/device.js";
-export { createBlobstoreRouter, chunkKey } from "./blobstore/index.js";
+export { createBlobstoreRouter, chunkKey, withinGracePeriod } from "./blobstore/index.js";
 export { pgSchema, sqliteSchema } from "./db/schema.js";
 export type {
   Chunk,
