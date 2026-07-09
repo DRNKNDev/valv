1d55077b398f522310cb17407fe3e4dc986fbcdd
DRNKN Master <master@drnkn.dev>
2026-07-09 13:04:30 +0700

    feat(api): run GC from cron

--- core/src/index.ts
+++ core/src/index.ts
@@ -16,13 +16,30 @@ export type {
 export { createSendInviteEmail } from "./email/index.js";
 export type { SendInviteEmail } from "./email/index.js";
 export {
+  DEFAULT_CHUNK_GC_BATCH_SIZE,
   DEFAULT_CHUNK_GC_INTERVAL_MS,
   DEFAULT_CHUNK_GRACE_PERIOD_MS,
+  DEFAULT_OPLOG_BATCH_SIZE,
   DEFAULT_OPLOG_RETENTION_MS,
   DEFAULT_OPLOG_TRUNCATION_INTERVAL_MS,
+  DEFAULT_TOMBSTONE_BATCH_SIZE,
   DEFAULT_TOMBSTONE_PURGE_INTERVAL_MS,
   DEFAULT_TOMBSTONE_RETENTION_MS,
+  UNBOUNDED_BATCH_SIZE,
+  runChunkGcOnce,
+  runOpLogTruncationOnce,
+  runTombstonePurgeOnce,
   startGc,
+} from "./gc/index.js";
+export type {
+  ChunkDeletionTarget,
+  GcMode,
+  GcOptions,
+  GcPassName,
+  GcPassResult,
+  RunChunkGcOnceArgs,
+  RunOpLogTruncationOnceArgs,
+  RunTombstonePurgeOnceArgs,
 } from "./gc/index.js";
 export { checkGrant, MAX_GRANT_WALK_DEPTH } from "./metadata/authz.js";
 export { createMetadataRouter } from "./metadata/index.js";
